fa16c390ca2e2913c90f1f9aef3400fd4871cd5e
sean o'connell <s.g.oconnell@gmail.com>
2021-05-12 21:53:17 -0400

    ARE YOU FUCKING SERIOUS? I WILL FITE YOU. DO ANOTHER COMMIT, I WILL FUCKING FITE YOU.

--- fitebawt.py
+++ fitebawt.py
@@ -1,23 +1,34 @@
-# IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
+# IMPORT
 import discord
+from fiteawth import DISCORD_TOKEN, BOT_TOKEN
 
-# IMPORT THE OS MODULE.
-import os
+# GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
+bot = discord.Client()
 
-# IMPORT LOAD_DOTENV FUNCTION FROM DOTENV MODULE.
-from dotenv import load_dotenv
+# EVENT LISTENER FOR WHEN THE BOT HAS SWITCHED FROM OFFLINE TO ONLINE.
+@bot.event
+async def on_ready():
+	# CREATES A COUNTER TO KEEP TRACK OF HOW MANY GUILDS / SERVERS THE BOT IS CONNECTED TO.
+	guild_count = 0
 
-# IMPORT COMMANDS FROM THE DISCORD.EXT MODULE.
-from discord.ext import commands
+	# LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+	for guild in bot.guilds:
+		# PRINT THE SERVER'S ID AND NAME.
+		print(f"- {guild.id} (name: {guild.name})")
 
-# LOADS THE .ENV FILE THAT RESIDES ON THE SAME LEVEL AS THE SCRIPT.
-load_dotenv()
+		# INCREMENTS THE GUILD COUNTER.
+		guild_count = guild_count + 1
 
-# GRAB THE API TOKEN FROM THE .ENV FILE.
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+	# PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
+	print("FiteBawt is in " + str(guild_count) + " guilds.")
 
-# CREATES A NEW BOT OBJECT WITH A SPECIFIED PREFIX. IT CAN BE WHATEVER YOU WANT IT TO BE.
-bot = commands.Bot(command_prefix="$")
+# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
+@bot.event
+async def on_message(message):
+	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
+	if message.content == "hello":
+		# SENDS BACK A MESSAGE TO THE CHANNEL.
+		await message.channel.send("hey dirtbag")
 
 # EXECUTES THE BOT WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
-bot.run(DISCORD_TOKEN)
+bot.run(BOT_TOKEN)
